140157147e9d3d296ddc6b0b331fbb8a682a6f86
Victor Santana <victorsantana.rezende@gmail.com>
2025-09-18 17:46:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,97 @@
 import flet as ft
 
 
-import flet as ft
-
 def main(page: ft.Page):
-    page.bgcolor = '#5B11BD'
+    page.bgcolor = "#D3D3D3"
     page.title = "MENU APP"
-    page.window_width = 500
-    page.window_height = 900
-    page.window_maximizable = False
+    page.window.width = 500
+    page.window.height = 900
+    page.window.max_width = 500
+    page.window.max_height = 900
+    
+    def handle_dismissal(e):
+        print(f"INICIO!")
+        print(f"Drawer dismissed!")
+    
+    
+    def handle_change(e):
+        print(f"Selected Index changed: {e.control.selected_index}")
+        page.close(drawer)
 
-    sidebar = ft.Container(
-        width=70,
-        bgcolor="#C50202",
-        border_radius=ft.border_radius.only(top_right=10, bottom_right=20),
-        content=ft.Column(
-            alignment=ft.MainAxisAlignment.START,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            controls=[
-                ft.IconButton(ft.Icons.HOME, icon_color="white"),
-                ft.IconButton(ft.Icons.SEARCH, icon_color="white"),
-                ft.IconButton(ft.Icons.FAVORITE, icon_color="white"),
-            ],
-            spacing=5
+    drawer = ft.NavigationDrawer(
+        on_dismiss=handle_dismissal,
+        on_change=handle_change,
+        controls=[
+            ft.Container(height=12),
+            ft.NavigationDrawerDestination(
+                label="INICIO",
+                icon=ft.Icons.HOME_ROUNDED,
+                selected_icon=ft.Icon(ft.Icons.HOME_ROUNDED),
+            ),
+            ft.Divider(thickness=2),
+            ft.NavigationDrawerDestination(
+                icon=ft.Icon(ft.Icons.NOTIFICATIONS_ACTIVE),
+                label="NOTIFICAÇÕES",
+                selected_icon=ft.Icons.NOTIFICATIONS_ACTIVE,
+            ),
+            ft.NavigationDrawerDestination(
+                icon=ft.Icon(ft.Icons.ALIGN_VERTICAL_BOTTOM_SHARP),
+                label="DESEMPENHO",
+                selected_icon=ft.Icons.ALIGN_VERTICAL_BOTTOM_SHARP,
+            ),ft.NavigationDrawerDestination(
+                label="AMIGOS",
+                icon=ft.Icons.MESSENGER,
+                selected_icon=ft.Icon(ft.Icons.MESSENGER),
+            ),ft.NavigationDrawerDestination(
+                label="SUPORTE",
+                icon=ft.Icon(ft.Icons.LIVE_HELP_ROUNDED),
+                selected_icon=ft.Icon(ft.Icons.LIVE_HELP_ROUNDED),
+            ),
+            ft.NavigationDrawerDestination(
+                label="CONFIGURAÇÕES",
+                icon=ft.Icon(ft.Icons.SETTINGS),
+                selected_icon=ft.Icon(ft.Icons.SETTINGS),
         ),
-        padding=ft.padding.only(left=0, top=10, right=10, bottom=10),  # Menos espaço à esquerda
-        margin=0,
-        expand=False,
+            ft.NavigationDrawerDestination(
+                label="SAIR",
+                icon=ft.Icon(ft.Icons.APP_BLOCKING),
+                selected_icon=ft.Icon(ft.Icons.APP_BLOCKING),
+            ),
+        ],
     )
+    def handle_dismissal(e):
+        print("End drawer dismissed")
 
-    page.add(
-        ft.Row(
-            controls=[
-                sidebar,
-                ft.Container(expand=True)  # Espaço para o conteúdo principal
-            ],
-            expand=True,
-            spacing=0  # Garante que não há espaço entre sidebar e conteúdo
-        )
+    def handle_change(e):
+        print(f"Selected Index changed: {e.control.selected_index}")
+        page.close(end_drawer)
+
+    end_drawer = ft.NavigationDrawer(
+        position=ft.NavigationDrawerPosition.END,
+        on_dismiss=handle_dismissal,
+        on_change=handle_change,
+        controls=[
+            ft.NavigationDrawerDestination(
+                icon=ft.Icons.ADD_TO_HOME_SCREEN_SHARP, label="Item 1"
+            ),
+            ft.NavigationDrawerDestination(
+                icon=ft.Icon(ft.Icons.ADD_COMMENT), label="Item 2"
+            ),
+            
+            ft.NavigationDrawerDestination(
+                icon=ft.Icon(ft.Icons.ADD_LOCATION), label="Item 3",
+        ),
+         
+
+        ],
     )
-
+    page.add(ft.Row([ft.IconButton(
+                    icon=ft.Icons.MENU,
+                    icon_color=ft.Colors.BLUE_900,
+                    icon_size=40,
+                    tooltip="Yep",
+                    on_click=lambda e: page.open(drawer),)
+                ],))
+    ####
+   
 ft.app(target=main)
